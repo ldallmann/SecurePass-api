14059cf92126b5edc4bbcf826ace5b5113f3f13b
Lucas <ldallmann.k@gmail.com>
2025-05-27 22:15:22 -0300

    correção na conexão com banco de dados

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 import mysql from "mysql";
 
 export const database = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "securepass",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
