0621c5b832c9bed7d37d85c187bd09740d9e599c
Lucas <ldallmann.k@gmail.com>
2025-05-27 22:24:00 -0300

    nova correção na conexão com banco de dados

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-import mysql from "mysql";
+import mysql from 'mysql2';
 
 export const database = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -6,4 +6,7 @@ export const database = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
