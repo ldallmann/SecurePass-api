03b8b74f2ae92adcc15510b4fa3c04d57788ead0
Lucas <ldallmann.k@gmail.com>
2025-05-27 22:29:00 -0300

    atualização de arquivo para conexão com banco de dados de deploy

--- database.js
+++ database.js
@@ -1,12 +1,22 @@
-import mysql from 'mysql2';
+const mysql = require('mysql2');
 
-export const database = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false
-    }
+const connection = mysql.createConnection({
+  host: 'bdgddgwdw0fbxittxv91-mysql.services.clever-cloud.com',
+  user: 'urc7ytvsihbdwnca',
+  password: 'seVgvk7tYEdGMlyI35OE',
+  database: 'bdgddgwdw0fbxittxv91',
+  port: 3306,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar no banco de dados:', err);
+  } else {
+    console.log('Conectado ao banco de dados MySQL com sucesso!');
+  }
+});
+
+module.exports = connection;
