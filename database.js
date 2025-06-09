53e34b897a6d729dbb80538bc1cc0249782abcf9
Lucas <ldallmann.k@gmail.com>
2025-06-09 20:34:00 -0300

    Novos dados de conexão ao bd

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 export const database = mysql.createConnection({
-  host: 'bdgddgwdw0fbxittxv91-mysql.services.clever-cloud.com',
-  user: 'urc7ytvsihbdwnca',
-  password: 'seVgvk7tYEdGMlyI35OE',
-  database: 'bdgddgwdw0fbxittxv91',
+  host: 'bsgpoholuauwiok2w54w-mysql.services.clever-cloud.com',
+  user: 'u6bzsm7j0ezvzewc',
+  password: 'lIuoAYgpPnqqF08LNzTO',
+  database: 'bsgpoholuauwiok2w54w',
   port: 3306,
   ssl: {
     rejectUnauthorized: false
