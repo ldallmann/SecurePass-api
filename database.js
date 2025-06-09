8d84abe578df012c2b36493af0f45a9aa9fabe59
Lucas <ldallmann.k@gmail.com>
2025-06-09 20:17:45 -0300

    Volta ao padrão de conexão BD

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 import mysql from 'mysql2';
 
-export const database = mysql.createPool({
+export const database = mysql.createConnection({
   host: 'bdgddgwdw0fbxittxv91-mysql.services.clever-cloud.com',
   user: 'urc7ytvsihbdwnca',
   password: 'seVgvk7tYEdGMlyI35OE',
   database: 'bdgddgwdw0fbxittxv91',
   port: 3306,
-  connectionLimit: 5,
   ssl: {
     rejectUnauthorized: false
   }
