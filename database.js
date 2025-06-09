47de021d051e37cc931793d6db5db5fd9005eac7
Lucas <ldallmann.k@gmail.com>
2025-06-09 20:03:53 -0300

    Problema na conexão com banco, limite de conexões

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2';
 
-export const database = mysql.createConnection({
+export const database = mysql.createPool({
   host: 'bdgddgwdw0fbxittxv91-mysql.services.clever-cloud.com',
   user: 'urc7ytvsihbdwnca',
   password: 'seVgvk7tYEdGMlyI35OE',
   database: 'bdgddgwdw0fbxittxv91',
   port: 3306,
+  connectionLimit: 5,
   ssl: {
     rejectUnauthorized: false
   }
